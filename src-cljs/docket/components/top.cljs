7cd173608425e89c8389747055dcb4f18ec368ef
Sheel Choksi <sheelchoksi@gmail.com>
2016-05-30 00:11:10 -0700

    Add a little stylist and more content to instances
    Instances are now expandable to show the tasks that are on that
    instance. Looks pretty terrible but neat to see work!

--- src-cljs/docket/components/top.cljs
+++ src-cljs/docket/components/top.cljs
@@ -1,33 +1,59 @@
 (ns docket.components.top
   (:require [reagent.core :as r]
             [docket.query-utils :as qu]
+            [clojure.string :as str]
             [datascript.core :as d]))
 
-
 (defn cluster-summary [cluster]
-  [:div
-   [:h1 "Cluster " (:cluster/cluster-name cluster)]
+  [:div.p2.bg-off-white
+   [:div.h1.mb2 "Cluster " (:cluster/cluster-name cluster)]
    [:div
     [:div "Running tasks: " (:cluster/running-tasks-count cluster)]
     [:div "Pending tasks: " (:cluster/pending-tasks-count cluster)]
     [:div "Registered container instances: " (:cluster/registered-container-instances-count cluster)]]])
 
-(defn instance-item [instance]
-  [:tr
-   [:td (:container-instance/ec2instance-id instance)]
-   [:td (:container-instance/running-tasks-count instance)]])
+(defn find-resource [name resources]
+  (first (filter (comp (partial = name) :name) resources)))
 
-(defn instances-list [instances]
-  [:div
-   [:h2 "Instances"]
-   [:table
-    [:thead
-     [:tr
-      [:th "EC2 Instance Id"]
-      [:th "Running Tasks Count"]]]
-    [:tbody
+(def remaining-memory (comp :integer-value (partial find-resource "MEMORY") :container-instance/remaining-resources))
+
+(defn instance-item [db instance]
+  (let [expanded (r/atom false)]
+    (fn [db instance]
+      [:div
+       [:div.clearfix.my1.pointer
+        {:on-click #(swap! expanded not)}
+        [:div.col.col-2 (:container-instance/ec2instance-id instance)]
+        (let [status (:container-instance/status instance)]
+          [:div.col.col-2 {:class (if (= "ACTIVE" status) "text-success" "text-warning")} status])
+        [:div.col.col-2 (:agent-version (:container-instance/version-info instance))]
+        [:div.col.col-2 (:docker-version (:container-instance/version-info instance))]
+        [:div.col.col-2 (:container-instance/running-tasks-count instance)]
+        [:div.col.col-2 (remaining-memory instance)]]
+       (when @expanded
+         [:div.col.col-12
+          [:div "Tasks"]
+          (for [task (qu/qes '[:find ?e
+                               :in $ ?c
+                               :where [?e :task/container-instance ?c]]
+                             @db (:db/id instance))]
+            ^{:key (:task/task-arn task)}
+            [:div (last (str/split (:task/task-definition-arn task) "/"))])])])))
+
+(defn instances-list [db instances]
+  [:div.my3
+   [:div.h2 "Instances"]
+   [:div.clearfix
+    [:div
+     [:div.col.col-2 "EC2 Instance Id"]
+     [:div.col.col-2 "Status"]
+     [:div.col.col-2 "Agent Version" ]
+     [:div.col.col-2 "Docker Version" ]
+     [:div.col.col-2 "Running Tasks Count"]
+     [:div.col.col-2 "Memory Remaining" ]]
+    [:div
      (for [instance instances]
-       ^{:key (:container-instance/container-instance-arn instance)} [instance-item instance])]]])
+       ^{:key (:container-instance/container-instance-arn instance)} [instance-item db instance])]]])
 
 (defn task-item [task]
   [:tr
@@ -35,8 +61,8 @@
 
 (defn tasks-list [tasks]
   [:div
-   [:h2 "Tasks"]
-   [:table
+   [:div.h2 "Tasks"]
+   [:table.table.table-hover
     [:thead
      [:tr]]
     [:tbody
@@ -47,7 +73,7 @@
   (let [db (:db @app)
         cluster (qu/qe-by @db :cluster/cluster-arn)]
     (when (seq cluster)
-      [:div
+      [:div.p4
        [cluster-summary cluster]
-       [instances-list (qu/qes-by @db :container-instance/container-instance-arn)]
+       [instances-list db (qu/qes-by @db :container-instance/container-instance-arn)]
        [tasks-list (qu/qes-by @db :task/task-arn)]])))
