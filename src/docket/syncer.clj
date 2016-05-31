f1628e48545610a958186a7d08ea3d5295c32ea8
Sheel Choksi <sheelchoksi@gmail.com>
2016-05-30 20:36:39 -0700

    Retract no longer present instances/tasks
    Actually removes these leftovers instances/tasks. We retract since in
    datascript that actually removes it and this is meant to be a cached set
    of data

--- src/docket/syncer.clj
+++ src/docket/syncer.clj
@@ -2,7 +2,7 @@
   (:require [com.stuartsierra.component :as component]
             [overtone.at-at :as at-at]
             [datascript.core :as d]
-            [clojure.set :refer [rename-keys]]
+            [clojure.set :refer [rename-keys difference]]
             [clojure.string :as str]
             [amazonica.aws.ecs :as ecs]))
 
@@ -25,29 +25,49 @@
   (prefix-keys "cluster"
                (first (:clusters (ecs/describe-clusters {:clusters [cluster-arn]})))))
 
-(defn get-container-instances [cluster-arn]
-  (let [instance-ids (:container-instance-arns (ecs/list-container-instances {:cluster cluster-arn}))]
-    (map (comp (partial add-refs {:container-instance/cluster (constantly [:cluster/cluster-arn cluster-arn])})
-            (partial prefix-keys "container-instance"))
-         (:container-instances (ecs/describe-container-instances {:container-instances instance-ids
-                                                                  :cluster cluster-arn})))))
+(defn retract-entity [lookup-ref lookup-val]
+  [:db.fn/retractEntity [lookup-ref lookup-val]])
 
-(defn get-tasks [cluster-arn]
-  (let [task-arns (:task-arns (ecs/list-tasks {:cluster cluster-arn}))]
-    (map (comp (partial add-refs {:task/cluster (constantly [:cluster/cluster-arn cluster-arn])
-                         :task/container-instance (partial rel-from-key
-                                                     :task/container-instance-arn
-                                                     :container-instance/container-instance-arn)})
-            (partial prefix-keys "task"))
-         (:tasks (ecs/describe-tasks {:tasks task-arns
-                                      :cluster cluster-arn})))))
+(defn resource-txns [app-state ident current-idents get-updates]
+  (let [prev-idents (map first (d/q '[:find ?lr :in $ ?ident :where [?e ?ident ?lr]] @app-state ident))
+        gone-idents (difference (set prev-idents) (set current-idents))
+        updates (get-updates current-idents)
+        retracts (map (partial retract-entity ident) gone-idents)]
+    (concat updates retracts)))
+
+(defn update-container-instance-txns [cluster-arn container-instance-arns]
+  (map (comp (partial add-refs {:container-instance/cluster (constantly [:cluster/cluster-arn cluster-arn])})
+          (partial prefix-keys "container-instance"))
+       (:container-instances (ecs/describe-container-instances {:container-instances container-instance-arns
+                                                                :cluster cluster-arn}))))
+
+(defn container-instance-txns [app-state cluster-arn]
+  (resource-txns app-state
+                 :container-instance/container-instance-arn
+                 (:container-instance-arns (ecs/list-container-instances {:cluster cluster-arn}))
+                 (partial update-container-instance-txns cluster-arn)))
+
+(defn update-task-txns [cluster-arn task-arns]
+  (map (comp (partial add-refs {:task/cluster (constantly [:cluster/cluster-arn cluster-arn])
+                       :task/container-instance (partial rel-from-key
+                                                   :task/container-instance-arn
+                                                   :container-instance/container-instance-arn)})
+          (partial prefix-keys "task"))
+       (:tasks (ecs/describe-tasks {:tasks task-arns
+                                    :cluster cluster-arn}))))
+
+(defn task-txns [app-state cluster-arn]
+  (resource-txns app-state
+                 :task/task-arn
+                 (:task-arns (ecs/list-tasks {:cluster cluster-arn}))
+                 (partial update-task-txns cluster-arn)))
 
 (defn sync-state [cluster-arn logger app-state]
   (try
     (d/transact! app-state
                  (concat [(get-cluster cluster-arn)]
-                         (get-container-instances cluster-arn)
-                         (get-tasks cluster-arn)))
+                         (container-instance-txns app-state cluster-arn)
+                         (task-txns app-state cluster-arn)))
     (catch Exception e
       (logger :error e))))
 
