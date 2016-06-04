43bb95c324a986469a56c2d5f2a5c48d9da75c8b
Sheel Choksi <sheelchoksi@gmail.com>
2016-06-04 12:55:30 -0700

    Add proper on-submit for modal form

--- src-cljs/docket/components/modal.cljs
+++ src-cljs/docket/components/modal.cljs
@@ -8,6 +8,10 @@
 (defn store-response [res-atom res]
   (reset! res-atom res))
 
+(defn prevent-default [f e]
+  (.preventDefault e)
+  (f))
+
 (defn modal [modal-display]
   (let [inputs (r/atom {})
         success-response (r/atom {})
@@ -21,6 +25,11 @@
            {:on-click #(reset! modal-display nil)}
            "Done"]]
          [:form
+          {:on-submit (partial prevent-default
+                         #(api/create-service
+                           (partial store-response success-response)
+                           (partial store-response failure-response)
+                           @inputs))}
           [:label.h3.block.mb1 "Service Name"]
           [:input.h3.block {:type "text" :on-change (partial store-input inputs :service-name)}]
 
@@ -30,9 +39,6 @@
           [:label.h3.block.mt3.mb1 "Task Definition"]
           [:input.h3.block {:type "text" :on-change (partial store-input inputs :task-def)}]
 
-          [:input.mt3 {:type "submit" :value "Create!" :on-click #(api/create-service
-                                                                   (partial store-response success-response)
-                                                                   (partial store-response failure-response)
-                                                                   @inputs)}]
+          [:input.mt3 {:type "submit" :value "Create!"}]
           (when-let [msg (:message @failure-response)]
             [:div.mt2.orange (pr-str msg)])])])))
